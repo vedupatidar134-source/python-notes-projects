c53037aa4c9d8041fa701416d7c2a90b929241ae
vedupatidar134-source <vedupatidar134@gmail.com>
2026-08-27 13:21:01 +0530

    :

---
+++ chapter 1/modules.py
@@ -0,0 +1,21 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# single line comments
+'''
+multi
+line
+comment
+'''
+
+
+"""
+also
+multi
+line
+comment
+"""
+
+# ctrl + / is short-cut to make single line comments 
